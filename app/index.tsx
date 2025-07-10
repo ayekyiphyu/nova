ce41bcdbc03d1330c6183d66f55952caed3003f4
ayekyiphyu <ayekyiphyu@yuime.co.jp>
2025-07-10 12:21:02 +0900

    add to the header and footer

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,21 @@
+import * as React from 'react';
 import { StyleSheet, Text, View } from "react-native";
+
 
 export default function Page() {
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Header Title</Text>
+      </View>
+
       <View style={styles.main}>
         <Text style={styles.title}>My React Expo</Text>
         <Text style={styles.subtitle}>This is the first page of your app.</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>© 2025 Develop by AYEKYIPHYU. All rights reserved.</Text>
       </View>
     </View>
   );
@@ -15,7 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    padding: 24,
+
   },
   main: {
     flex: 1,
@@ -27,8 +37,36 @@ const styles = StyleSheet.create({
     fontSize: 64,
     fontWeight: "bold",
   },
+
   subtitle: {
     fontSize: 36,
     color: "#38434D",
+  },
+
+  header: {
+    width: '100%',
+    paddingVertical: 12,
+    backgroundColor: '#f2f2f2',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+
+  footer: {
+    width: '100%',
+    paddingVertical: 12,
+    backgroundColor: '#f2f2f2',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+  },
+  footerText: {
+    fontSize: 14,
+    color: '#666',
+  },
+
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: '600',
   },
 });
